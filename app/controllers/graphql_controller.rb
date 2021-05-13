d6f18c1f863fe96b37817de968b313dbd0fcc6b6
takashi oka <takashi.oka@gmail.com>
2021-05-13 15:03:21 +0900

    Graphql_controller:revised

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -6,7 +6,7 @@ class GraphqlController < ApplicationController
     context = {
       # Query context goes here, for example:
       # current_user: current_user,
-      blog: Blog.last
+      blog: Blog.first
     }
     result = WebTest3Schema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
